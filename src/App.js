5ccb0ed61c9e53e2c9f4f73ce22488aa64575989
ankitdatta17 <ankitdatta04@gmail.com>
2025-07-29 17:53:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import CricketScore from './component/CricketScore';
+ 
+function App() {
+  return (
+    <CricketScore/>
+  );
+}
+
+export default App;
